df4816c38fe7450769d4b5cd77f267f57b811d9f
Erik Ford <ekford18@gmail.com>
2020-06-04 15:35:15 -0700

    tests completed

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const { MongoMemoryServer } = require('mongodb-memory-server');
 const request = require('supertest');
 const app = require('../lib/app');
-const Character = require('../lib/models/Character');
+const Character = require('../lib/Character');
 
 describe('app routes', () => {
   const mongo = new MongoMemoryServer();
@@ -31,7 +31,7 @@ describe('app routes', () => {
         level: 8
       })
       .then(res => {
-        expect.res.body.toEqual({
+        expect(res.body).toEqual({
           _id: expect.anything(),
           name: 'Morriden',
           description: 'A Drow who uses magic to help those in need.',
@@ -41,5 +41,106 @@ describe('app routes', () => {
       });
   });
 
+  it('gets one character by id', async() => {
+
+    const characterId = await Character.create({
+      name: 'Morriden',
+      description: 'A Drow who uses magic to help those in need.',
+      level: 8,
+    });
+
+    return request(app)
+      .get(`/characters/${characterId._id}`)
+      .then(res => {
+        expect(res.body).toEqual(
+          {
+            _id: expect.anything(),
+            name: 'Morriden',
+            description: 'A Drow who uses magic to help those in need.',
+            level: 8,
+            __v: 0           
+          }
+        );
+      });
+
+  });
+
+  it('get a list of characters', async() => {
+    await Character.create(
+      {
+        name: 'Morriden',
+        description: 'A Drow who uses magic to help those in need.',
+        level: 8,
+      },
+      {
+        name: 'Kartanis',
+        description: 'A Dragonborn paladin who praises Bahamut and protects the weak!',
+        level: 8,
+      }
+    );
+
+    return request(app)
+      .get('/characters')
+      .then(res => {
+        expect(res.body).toEqual([
+          {
+            _id: expect.anything(),
+            name: 'Morriden',
+            description: 'A Drow who uses magic to help those in need.',
+            level: 8,
+            __v: 0 
+          },
+          {
+            _id: expect.anything(),
+            name: 'Kartanis',
+            description: 'A Dragonborn paladin who praises Bahamut and protects the weak!',
+            level: 8,
+            __v: 0 
+          }
+        ]);
+      });
+  });
+
+  it('updates the level of a character', async() => {
+    const characterId = await Character.create(
+      {
+        name: 'Morriden',
+        description: 'A Drow who uses magic to help those in need.',
+        level: 8,
+      });
+
+    return request(app)
+      .patch(`/characters/update/${characterId._id}/10`)
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.anything(),
+          name: 'Morriden',
+          description: 'A Drow who uses magic to help those in need.',
+          level: 10,
+          __v: 0
+        });
+      });
+  });
+
+  it('deletes the character', async() => {
+    const characterId = await Character.create(
+      {
+        name: 'Morriden',
+        description: 'A drow who uses magic.',
+        level: 8,
+      });
+
+    return request(app)
+      .delete(`/characters/delete/${characterId._id}`)
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.anything(),
+          name: 'Morriden',
+          description: 'A drow who uses magic.',
+          level: 8,
+          __v: 0
+        });
+      });
+  });
 
 });
